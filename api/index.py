43390fa67798fb2bf21ebca9f05ea190dc17d741
BitJian <1183048003@qq.com>
2023-12-14 22:54:29 +0800

    部署

--- api/index.py
+++ api/index.py
@@ -9,8 +9,8 @@ def list_split(items, n):
 def getdata(name):
     gitpage = requests.get("https://github.com/" + name)
     data = gitpage.text
-    datadatereg = re.compile(r'data-date="(.*?)" data-level')
-    datacountreg = re.compile(r'rx="2" ry="2">(.*?) contribution')
+    datadatereg = re.compile(r'data-date="(.*?)" id="contribution-day-component')
+    datacountreg = re.compile(r'<tool-tip .*?class="sr-only position-absolute">(.*?) contribution')
     datadate = datadatereg.findall(data)
     datacount = datacountreg.findall(data)
     datacount = list(map(int, [0 if i == "No" else i for i in datacount]))
@@ -31,8 +31,13 @@ class handler(BaseHTTPRequestHandler):
         user = path.split('?')[1]
         data = getdata(user)
         self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Origin', 'https://blog.bitjian.cn')
         self.send_header('Content-type', 'application/json')
         self.end_headers()
         self.wfile.write(json.dumps(data).encode('utf-8'))
         return
+if __name__ == '__main__':
+    from http.server import ThreadingHTTPServer
+    server = ThreadingHTTPServer(('localhost', 3006), handler)
+    print('Starting server on localhost:3006')
+    server.serve_forever()
